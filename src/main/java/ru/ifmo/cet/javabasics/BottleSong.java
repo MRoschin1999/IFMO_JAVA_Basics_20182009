b7c427b191a43f5cb341495d8cd1e0479b21a37b
MRoschin1999 <roschin.1999@mail.ru>
2018-09-30 09:47:50 +0300

    final change

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -31,13 +31,136 @@ package ru.ifmo.cet.javabasics;
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
 public class BottleSong {
+    int btao;
 
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        this.btao = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
         //TODO
-        throw new UnsupportedOperationException();
+        if ((this.btao < 100) & (this.btao > 0))
+        {
+            String final1 = "";
+            Integer bottles = 99;
+            Integer tmpBottles = 99;
+            String strbtao = getBottle(this.btao);
+            while (bottles > this.btao) {
+                tmpBottles = bottles - this.btao;
+                if (tmpBottles>1) {
+                    final1 += bottles.toString() + " bottles of beer on the wall, " + bottles.toString() + " bottles of beer.\n" +
+                            "Take " + strbtao + "down and pass around, " + tmpBottles.toString() + " bottles of beer on the wall.\n";
+                }
+                else{
+                    final1 += bottles.toString() + " bottles of beer on the wall, " + bottles.toString() + " bottles of beer.\n" +
+                            "Take " + strbtao + "down and pass around, " + tmpBottles.toString() + " bottle of beer on the wall.\n";
+                }
+                bottles = tmpBottles;
+            }
+            if ( bottles > 1) {
+                final1 += bottles.toString() + " bottles of beer on the wall, " + bottles.toString() + " bottles of beer.\n" +
+                        "Take " + getBottle(bottles) + "down and pass around, no more bottles of beer on the wall.\n" +
+                        "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+            }
+            else{
+                final1 += bottles.toString() + " bottle of beer on the wall, " + bottles.toString() + " bottle of beer.\n" +
+                        "Take " + getBottle(bottles) + "down and pass around, no more bottles of beer on the wall.\n" +
+                        "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+            }
+            return final1;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public String getBottle(int number){
+        String str = "";
+        if(number < 20) {
+            switch (number) {
+                case 1: str = "one ";
+                    break;
+                case 2: str = "two ";
+                    break;
+                case 3: str = "three ";
+                    break;
+                case 4: str = "four ";
+                    break;
+                case 5: str = "five ";
+                    break;
+                case 6: str = "six ";
+                    break;
+                case 7: str = "seven ";
+                    break;
+                case 8: str = "eight ";
+                    break;
+                case 9: str = "nine ";
+                    break;
+                case 10: str = "ten ";
+                    break;
+                case 11: str = "eleven ";
+                    break;
+                case 12: str = "twelve ";
+                    break;
+                case 13: str = "thirteen ";
+                    break;
+                case 14: str = "fourteen ";
+                    break;
+                case 15: str = "fifteen ";
+                    break;
+                case 16: str = "sixteen ";
+                    break;
+                case 17: str = "seventeen ";
+                    break;
+                case 18: str = "eighteen ";
+                    break;
+                case 19: str = "nineteen ";
+                    break;
+            }
+        }
+        else{
+            switch (number / 10) {
+                case 2: str = "twenty ";
+                    break;
+                case 3: str = "thirty ";
+                    break;
+                case 4: str = "forty ";
+                    break;
+                case 5: str = "fifty ";
+                    break;
+                case 6: str = "sixty ";
+                    break;
+                case 7: str = "seventy ";
+                    break;
+                case 8: str = "eighty ";
+                    break;
+                case 9: str = "ninety ";
+                    break;
+            }
+            switch (number % 10) {
+                case 0: str += "";
+                    break;
+                case 1: str += "one ";
+                    break;
+                case 2: str += "two ";
+                    break;
+                case 3: str += "three ";
+                    break;
+                case 4: str += "four ";
+                    break;
+                case 5: str += "five ";
+                    break;
+                case 6: str += "six ";
+                    break;
+                case 7: str += "seven ";
+                    break;
+                case 8: str += "eight ";
+                    break;
+                case 9: str += "nine ";
+                    break;
+            }
+        }
+        return str;
     }
 }
