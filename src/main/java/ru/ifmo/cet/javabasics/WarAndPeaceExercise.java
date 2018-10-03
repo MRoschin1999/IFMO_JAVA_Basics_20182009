a32d0a4e7a5e19972d648caf813d3d8b4b675871
MRoschin1999 <roschin.1999@mail.ru>
2018-10-03 17:15:35 +0300

    final change

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -36,22 +36,22 @@ public class WarAndPeaceExercise {
         subStr.addAll(Arrays.asList(subStr2));
         Map<String, Integer> words = new HashMap<>();
 
-        for (int i = 0; i < subStr.size(); i++){
-            if((subStr.get(i).length() > 3)) {
-                if (!words.containsKey(subStr.get(i)))
-                    words.put(subStr.get(i), 1);
+        for (String aSubStr : subStr) {
+            if ((aSubStr.length() > 3)) {
+                if (!words.containsKey(aSubStr))
+                    words.put(aSubStr, 1);
                 else {
-                    words.put(subStr.get(i), words.get(subStr.get(i)) + 1);
+                    words.put(aSubStr, words.get(aSubStr) + 1);
                 }
             }
         }
 
         List<Map.Entry<String,Integer>> sortMap = new ArrayList<>(words.entrySet());
 
-        Collections.sort(sortMap, new Comparator<Map.Entry<String, Integer>>() {
+        sortMap.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if(o1.getValue().equals(o2.getValue()))
+                if (o1.getValue().equals(o2.getValue()))
                     return o1.getKey().compareTo(o2.getKey());
                 else
                     return o2.getValue().compareTo(o1.getValue());
@@ -64,10 +64,17 @@ public class WarAndPeaceExercise {
         for(Map.Entry entry: sortMap){
             String key = (String) entry.getKey();
             Integer value = (Integer) entry.getValue();
-            if (value >= 10)
+            if (value >= 10) {
                 result += key + " - " + value + "\n";
+            }
         }
+        // TODO map lowercased words to its amount in text and concatenate its entries.
+        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
+        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
+        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        //System.out.println(result);
         return result.substring(0, result.length() - 1);
+        //throw new UnsupportedOperationException(result);
     }
 
 }
