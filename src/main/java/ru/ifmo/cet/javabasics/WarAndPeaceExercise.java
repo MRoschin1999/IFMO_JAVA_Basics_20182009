6aa2c3297a3fa392dbb15c05084a4eb3c0566711
MRoschin1999 <roschin.1999@mail.ru>
2018-10-03 19:35:10 +0300

    FINAL

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,29 +1,19 @@
 package ru.ifmo.cet.javabasics;
-import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.FileReader;
-import java.util.*;
-import java.io.File;
-import java.io.FileReader;
 
 import static java.nio.file.Files.readAllLines;
-
 
 public class WarAndPeaceExercise {
     public static Map<String, Integer> words = new HashMap<>();
     public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-        findWord(tome12Path);
-        findWord(tome34Path);
+        find(tome12Path);
+        find(tome34Path);
 
         List<Map.Entry<String,Integer>> sortMap = new ArrayList<>(words.entrySet());
 
@@ -46,10 +36,9 @@ public class WarAndPeaceExercise {
             }
         }
         return result.trim();
-        //throw new UnsupportedOperationException(result.substring(0,result.length() - 1));
     }
 
-    private static void findWord(Path path) throws IOException {
+    private static void find(Path path) throws IOException {
         for (String line : readAllLines(path, Charset.forName("windows-1251"))) {
             line = line.replaceAll("[^а-яА-Яa-zA-Z]", " ");
             for (String word : line.split(" ")) {
