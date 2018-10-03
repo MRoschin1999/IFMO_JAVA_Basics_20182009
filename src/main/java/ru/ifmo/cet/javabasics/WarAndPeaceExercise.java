5918982d5c66996271cfa24141a74c96c586f30c
MRoschin1999 <roschin.1999@mail.ru>
2018-10-03 16:58:35 +0300

    final change

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,73 @@
 package ru.ifmo.cet.javabasics;
 
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.FileReader;
+import java.util.*;
+import java.io.File;
+import java.io.FileReader;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        String strPath12 = tome12Path.toString();
+        String content12 = new String(Files.readAllBytes(Paths.get(strPath12)));
+        content12 = content12.toLowerCase();
 
-        throw new UnsupportedOperationException();
+        String strPath34 = tome34Path.toString();
+        String content34 = new String(Files.readAllBytes(Paths.get(strPath34)));
+        content34 = content34.toLowerCase();
+
+        String[] subStr1;
+        subStr1 = content12.split("[^a-zA-Zа-яА-Я]");
+        String[] subStr2;
+        subStr2 = content34.split("[^a-zA-Zа-яА-Я]");
+
+        ArrayList<String> subStr = new ArrayList<>();
+
+        subStr.addAll(Arrays.asList(subStr1));
+        subStr.addAll(Arrays.asList(subStr2));
+        Map<String, Integer> words = new HashMap<>();
+
+        for (int i = 0; i < subStr.size(); i++){
+            if((subStr.get(i).length() > 3)) {
+                if (!words.containsKey(subStr.get(i)))
+                    words.put(subStr.get(i), 1);
+                else {
+                    words.put(subStr.get(i), words.get(subStr.get(i)) + 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String,Integer>> sortMap = new ArrayList<>(words.entrySet());
+
+        Collections.sort(sortMap, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o1.getValue().equals(o2.getValue()))
+                    return o1.getKey().compareTo(o2.getKey());
+                else
+                    return o2.getValue().compareTo(o1.getValue());
+
+            }
+        });
+
+        String result = "";
+
+        for(Map.Entry entry: sortMap){
+            String key = (String) entry.getKey();
+            Integer value = (Integer) entry.getValue();
+            if (value >= 10)
+                result += key + " - " + value + "\n";
+        }
+        return result.substring(0, result.length() - 1);
     }
 
 }
